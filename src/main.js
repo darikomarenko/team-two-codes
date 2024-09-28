69565ce4fdfbfc4f5060d7ba0d985e72ce2fd860
eevitg <vketh@yandex.ru>
2024-09-28 18:29:36 +0300

    add map

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import { renderHeader } from './components/Header.js';
+import { renderMap } from './components/Map.js';
 import { renderFooter } from './components/Footer.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   renderHeader();
+  renderMap();
   renderFooter();
 });
