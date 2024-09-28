e36ee5e5de8d5db12fb9b46a85168cc9f9a412b8
Daria Komarenko <dkomarenko@spherical.pm>
2024-09-28 21:24:41 +0300

    Add Heeader and Banner components

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import { renderHeader } from './components/Header.js';
 import { renderFooter } from './components/Footer.js';
+import { renderBanner } from './components/Banner.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   renderHeader();
   renderFooter();
+  renderBanner();
 });
