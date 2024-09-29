c07cf4d089fb2231e6b52a32498fb773a4422cc2
HelenRed <131916662+Chuchundra009@users.noreply.github.com>
2024-09-29 12:53:25 +0300

    add elements in block whataboul

--- src/components/WhatAbout.js
+++ src/components/WhatAbout.js
@@ -1,5 +1,29 @@
 import '../assets/styles/whatAbout.scss';
 
 export function renderWhatAbout() {
-    
+    const whatAboutOur= document.getElementById('whatAboutOur');
+
+    whatAboutOur.innerHTML= /*html*/`
+    <div>
+          <div class="whatAbout__block">
+            <div class="whatAbout__block-teame">
+              <p class="whatAbout__block-chapter"><span class="whatAbout__block-span">—</span> About Us</p>
+              <h2 class="whatAbout__block-title">What about Our Industry Performance.</h2>
+            </div>
+            <div class="whatAbout__block-description">
+              <p class="whatAbout__block-descriptionText">In today's world, we're bombarded with choices. From streaming services to meal kits, it can be overwhelming.</p>
+              <div class="whatAbout__block-section">
+                <button class="whatAbout__block-btn">Read More<img src="../../public/images/WhatAbout/arrow-white.png" class="whatAbout__block-btnImg" alt="arrow white"></button>
+                <div>
+                    <p class="whatAbout__block-portrait">25+ Experience</p>
+                    <p class="whatAbout__reflection">25+ Experience</p>
+                </div>
+                
+              </div>
+            </div>
+          </div>
+          <div class="whatAbout__slider"></div>
+        </div>
+        <img src="../../public/images/WhatAbout/Architecture.png" class="whatAbout__architecture" alt="Architecture right">
+    `
 }
